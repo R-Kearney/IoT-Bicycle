ca16af5d4e5f213a08f35aedb977b79409515c73
Ricky Kearney <14141647@studentmail.ul.ie>
2018-01-31 12:58:04 +0000

    Pycom - Anti-Theft Tracking Improvemenst

--- Anti-Theft and Tracking/boot.py
+++ Anti-Theft and Tracking/boot.py
@@ -2,8 +2,13 @@ import network
 import time
 # setup as a station
 wlan = network.WLAN(mode=network.WLAN.STA)
-wlan.connect('Wifi Network Name', auth=(network.WLAN.WPA2, 'Password'))
+wlan.connect('SSID', auth=(network.WLAN.WPA2, 'PASSWORD'))
+i = 0
 while not wlan.isconnected():
     time.sleep_ms(50)
+    i += 1
+    if (i == 3): # try to connect to WiFi 3 times before giving up
+        break
 print(wlan.ifconfig())
+
 # now use socket as usual
--- Anti-Theft and Tracking/main.py
+++ Anti-Theft and Tracking/main.py
@@ -2,7 +2,7 @@ from pytrack import Pytrack
 from LIS2HH12 import LIS2HH12
 import pycom
 import time
-from network import Bluetooth
+#from network import Bluetooth
 # import network
 from network import Sigfox
 # import machine
@@ -17,12 +17,14 @@ import gc
 from machine import RTC
 
 pycom.heartbeat(False)
-wdt = WDT(timeout=60000)  # enable it with a timeout of 1 minute
+wdt = WDT(timeout=60000)  # enable Watchdog with a timeout of 1 minute
 
 py = Pytrack()
+# enable wakeup source from INT pin
+py.setup_int_pin_wake_up(False)
 py.setup_int_wake_up(True, True)
 acc = LIS2HH12()
-acc.enable_activity_interrupt(200, 20)
+acc.enable_activity_interrupt(100, 20) # Very sensitive
 
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1) # init Sigfox for RCZ1 (Europe)
 s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW) # create a Sigfox socket
@@ -30,48 +32,51 @@ s.setblocking(True) # make the socket blocking
 s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False) # configure it as uplink only
 sigfoxCounter = 0 # count numbre of sigfox trnasmissions (max 140 a day)
 
-
-time.sleep(2)
 gc.enable()
-
-
 l76 = L76GNSS(py, timeout=30) # setup GPS
 
-# check if we were awaken due to activity
-if acc.activity():
-    gc.collect()
-    noLock = 0
-    lock = False
-    pycom.rgbled(0xFF0000) # red
-    print("Activity")
-    # print("Up Time %i seconds" % utime.time())
+gc.collect()
+noLock = 0
+lock = False
+pycom.rgbled(0xFF0000) # red
+print("Activity")
+# display the reset reason code and the sleep remaining in seconds
+# possible values of wakeup reason are:
+# WAKE_REASON_ACCELEROMETER = 1
+# WAKE_REASON_PUSH_BUTTON = 2
+# WAKE_REASON_TIMER = 4
+# WAKE_REASON_INT_PIN = 8
+print("Wakeup reason: " + str(py.get_wake_reason()) + "; Aproximate sleep remaining: " + str(py.get_sleep_remaining()) + " sec")
 
-    while (lock == False) and (noLock < 2): # Try get GPS 3 Times before giving up and going to Sleep
-        coord = l76.coordinates(debug = False)
-        #f.write("{} - {}\n".format(coord, rtc.now()))
-        print("{} - {} KB".format(coord, gc.mem_free()/1000))
-        if not all(coord): # returns false if none is in the truple
-            print("No lock found")
-            noLock = noLock + 1
-            wdt.feed() # tell watchdog were stll alive
-        else:
-            print("lock found")
-            lock = True
-            wdt.feed() # tell watchdog were stll alive
-            #coordStr = ' '.join(map(str,float_to_hex(coord)))
-            coordHex = (hex(int(coord[0] * 100000))[2:] + hex(int(coord[1] * 100000))[2:]) # 5 dec places is plenty acctraue. (4 is still good)
-            messageToSend = coordHex
-            tempMessage = []
-            bytesToSend = 0
-            lastCount = 0
-            while bytesToSend < len(messageToSend):
-                bytesToSend = bytesToSend + 12
-                s.send(str(messageToSend[lastCount:bytesToSend]))
-                lastCount = bytesToSend
-                sigfoxCounter = sigfoxCounter + 1
-                print("Sigfox messages sent: ", sigfoxCounter)
-            time.sleep(5) # let everything go through
 
-# go to sleep for 5 minutes maximum if no accelerometer interrupt happens
-py.setup_sleep(300)
+while (lock == False) and (noLock < 10): # Try get GPS 3 Times before giving up and going to Sleep
+    coord = l76.coordinates(debug = False)
+    #f.write("{} - {}\n".format(coord, rtc.now()))
+    print("{} - {} KB".format(coord, gc.mem_free()/1000))
+    if not all(coord): # returns false if none is in the truple
+        print("No lock found")
+        noLock = noLock + 1
+        wdt.feed() # tell watchdog we're stll alive
+    else:
+        print("lock found")
+        lock = True
+        wdt.feed() # tell watchdog were stll alive
+        #coordStr = ' '.join(map(str,float_to_hex(coord)))
+        coordHex = (hex(int(coord[0] * 100000))[2:] + hex(int(coord[1] * 100000))[2:]) # 5 dec places is plenty acctraue. (4 is still good)
+        messageToSend = coordHex
+        tempMessage = []
+        bytesToSend = 0
+        lastCount = 0
+        while bytesToSend < len(messageToSend):
+            bytesToSend = bytesToSend + 12
+            s.send(str(messageToSend[lastCount:bytesToSend]))
+            lastCount = bytesToSend
+            sigfoxCounter = sigfoxCounter + 1
+            print("Sigfox messages sent: ", sigfoxCounter)
+        time.sleep(5) # let everything go through
+
+# go to sleep for 24 hours maximum if no accelerometer interrupt happens
+print("Wakeup reason: " + str(py.get_wake_reason()) + "; Aproximate sleep remaining: " + str(py.get_sleep_remaining()) + " sec")
+print("Going to sleep")
+py.setup_sleep(86400)
 py.go_to_sleep()
