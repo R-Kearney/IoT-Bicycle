d05e13d9032f13d386ab3457999e681aeaeefff6
Ricky Kearney <14141647@studentmail.ul.ie>
2018-03-03 12:23:09 +0000

    Pycom - GPS location timeout increased

--- Anti-Theft and Tracking/main.py
+++ Anti-Theft and Tracking/main.py
@@ -24,7 +24,7 @@ py = Pytrack()
 py.setup_int_pin_wake_up(False)
 py.setup_int_wake_up(True, True)
 acc = LIS2HH12()
-acc.enable_activity_interrupt(100, 20) # Very sensitive
+acc.enable_activity_interrupt(100, 200) # Very sensitive
 maxSleep = 86400 # Max time the device is allowed to sleep for (seconds). 24 hours
 
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1) # init Sigfox for RCZ1 (Europe)
@@ -34,7 +34,7 @@ s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False) # configure it as uplink on
 sigfoxCounter = 0 # count numbre of sigfox trnasmissions (max 140 a day)
 
 gc.enable()
-l76 = L76GNSS(py, timeout=30) # setup GPS
+l76 = L76GNSS(py, timeout=60) # setup GPS
 
 gc.collect()
 noLock = 0
@@ -44,24 +44,20 @@ print("Activity")
 asleepTime = maxSleep - py.get_sleep_remaining() # seconds, not accurate
 
 print("We were asleep for ", asleepTime , " Seconds.")
-if (asleepTime < 200): # limit the max frequency of location pulls
-    time.sleep(60)
 
-while (lock == False) and (noLock < 3): # Try get GPS 3 Times before giving up and going to Sleep
+while (lock == False) and (noLock < 6): # Try get GPS 6 Times before giving up and going to Sleep
     coord = l76.coordinates(debug = False)
     print("{} - {} KB".format(coord, gc.mem_free()/1000))
     if not all(coord): # returns false if none is in the truple
-        print("No lock found")
+        print(noLock , " - No lock found")
         noLock = noLock + 1
         wdt.feed() # tell watchdog we're stll alive
     else:
         print("lock found")
         lock = True
         wdt.feed() # tell watchdog were stll alive
-        #coordStr = ' '.join(map(str,float_to_hex(coord)))
-        coordHex = (hex(int(coord[0] * 100000))[2:] + hex(int(coord[1] * 100000))[2:]) # 5 dec places is plenty acctraue. (4 is still good)
+        coordHex = (hex(int(coord[0] * 100000))[2:] + hex(int(coord[1] * 100000))[2:]) # 5 dec places is plenty accurate. (4 is still good)
         messageToSend = coordHex
-        tempMessage = []
         bytesToSend = 0
         lastCount = 0
         while bytesToSend < len(messageToSend):
